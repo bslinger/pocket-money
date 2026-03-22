10819faccfdd1ba05d0cc23be8c935f9d0d79cdc
bslinger <ben@podchaser.com>
2026-03-22 22:56:13 +1100

    Fix SSR by adding Ziggy route() to SSR entry
    Import ziggy-js in ssr.tsx and set globalThis.route so server
    rendering can resolve route() calls. Add ziggy-js alias to Vite
    config and install qs-esm dependency.

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,4 +1,9 @@
 import { createInertiaApp } from '@inertiajs/react';
+import { route as ziggyRoute } from 'ziggy-js';
+import { Ziggy } from './ziggy';
+
+globalThis.route = ((name: string, params?: any, absolute?: boolean) =>
+    ziggyRoute(name, params, absolute, Ziggy as any)) as typeof ziggyRoute;
 
 const appName = import.meta.env.VITE_APP_NAME || 'Quiddo';
 
